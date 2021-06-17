162d208b0dd51b3347d081729712e49f6abd15e5
MVaraPrasad <varaprasad.512@gmail.com>
2021-06-17 14:18:50 +0530

    Quick Initial Updates

---
+++ src/components/Counter.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+class Counter extends Component {
+    render() { 
+        return <h1>This is a Counter component</h1>;
+    }
+}
+ 
+export default Counter ;
